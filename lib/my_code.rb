aac2c635a47a6239639a97119346249c938de470
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 19:00:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,12 @@
 # Your Code Here
 
 def map(source_array) 
-  negative_values = []
+  values = []
   i = 0 
   while i < source_array.length
-    negative_values.push(yield(source_array[i]))
+    values.push(yield(source_array[i]))
     i += 1 
   end
-  return negative_values
+  return values
 end
 
