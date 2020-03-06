1f6166e39d4a9fe93f16f23f4987567730f55cb1
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 19:12:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ end
 def reduce(source_array, starting_value = 0)
   i = 0 
   while i < source_array.length
-    starting_value.memo()
+    starting_value = yield(starting_value, )
     i += 1 
   end
   
