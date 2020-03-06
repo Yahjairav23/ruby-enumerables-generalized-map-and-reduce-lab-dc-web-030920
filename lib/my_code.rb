883ddba3b3e4ef3bf7ca6c6ea9a1175a89f57d95
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 18:58:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,11 +5,8 @@ def map(source_array)
   i = 0 
   while i < source_array.length do
     negative_values.push(yield(source_array[1]))
-    
     i += 1 
   end
-  yield(negative_values)
   return negative_values
 end
 
-map(array) {|n| n * -1}
