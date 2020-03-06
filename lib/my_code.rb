35348eb2b364c51cc8d92f407e5a00a1cbb6302c
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 19:17:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,8 +10,15 @@ def map(source_array)
   return values
 end
 
-def reduce(source_array, starting_value = 0)
-  i = 0 
+def reduce(source_array, starting_value = nil)
+  if starting_value
+    sum = starting_value
+    i = 0
+  else
+    sum = source_array[0]
+    i = 1 
+  end
+  
   while i < source_array.length
     starting_value = yield(starting_value, source_array[i])
     i += 1 
